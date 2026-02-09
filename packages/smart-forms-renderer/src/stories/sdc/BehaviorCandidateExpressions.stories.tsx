74b816e5a82c38f4e0090d4b6e21d7cd5a15dd30
IlonaProjects <ilona.weber@cistec.com>
2026-02-09 19:18:32 +0100

    Lint Cleanup

--- packages/smart-forms-renderer/src/stories/sdc/BehaviorCandidateExpressions.stories.tsx
+++ packages/smart-forms-renderer/src/stories/sdc/BehaviorCandidateExpressions.stories.tsx
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 import type { Questionnaire } from 'fhir/r4';
 import BaseRenderer from '../../components/Renderer/BaseRenderer';
 
